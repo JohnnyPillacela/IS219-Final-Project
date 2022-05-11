01647186c4ecb50d5e2860f7a25afbb4442ab075
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-11 15:53:46 -0400

    edit view

--- app/cars/__init__.py
+++ app/cars/__init__.py
@@ -6,7 +6,7 @@ from app import db
 from app.db import db
 from app.auth import admin_required
 from app.db.models import Cars
-from app.cars.forms import create_car_form
+from app.cars.forms import create_car_form, edit_car_form
 
 cars = Blueprint('cars', __name__,
                  template_folder='templates')
@@ -62,3 +62,32 @@ def add_car():
     #     flash('new product')
     #     return redirect(url_for('cars.browse_cars'))
     return render_template('add_car.html', form=form)
+
+
+@cars.route('/car/<int:car_id>/delete', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def delete_car(car_id):
+    car = Cars.query.get(car_id)
+    db.session.delete(car)
+    db.session.commit()
+    flash('Car Deleted', 'success')
+    return redirect(url_for('cars.browse_cars'))
+
+
+@cars.route('/car/<int:car_id>/edit', methods=['POST', 'GET'])
+@login_required
+@admin_required
+def edit_location(car_id):
+    car = Cars.query.get(car_id)
+    form = edit_car_form(obj=car)
+    if form.validate_on_submit():
+        car.price = form.price.data
+        if form.description.data is not None:
+            car.description = form.description.data
+        db.session.add(car)
+        db.session.commit()
+        flash("Car Edited Successfully", "success")
+        current_app.logger.info("edited a car")
+        return redirect(url_for("cars.list_cars"))
+    return render_template("edit_car.html", form=form)
