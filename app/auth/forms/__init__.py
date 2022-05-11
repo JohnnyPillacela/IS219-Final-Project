20a2943a96e9a2922d52d362ab27a4a009fe1918
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-11 01:03:21 -0400

    added validation

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -18,16 +18,18 @@ class login_form(FlaskForm):
 class register_form(FlaskForm):
     email = EmailField('Email Address', [
         validators.DataRequired(),
-
+        validators.Email()
     ], description="You need to signup with an email")
 
     password = PasswordField('Create Password', [
         validators.DataRequired(),
+        validators.length(min=6, max=35, message="Field must be between 6 and 35 characters long."),
         validators.EqualTo('confirm', message='Passwords must match'),
 
     ], description="Create a password ")
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
     submit = SubmitField()
+
 
 class create_user_form(FlaskForm):
     email = EmailField('Email Address', [
@@ -41,10 +43,8 @@ class create_user_form(FlaskForm):
 
     ], description="Create a password ")
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
-    is_admin = BooleanField('Admin', render_kw={'value':'1'})
+    is_admin = BooleanField('Admin', render_kw={'value': '1'})
     submit = SubmitField()
-
-
 
 
 class profile_form(FlaskForm):
@@ -53,10 +53,11 @@ class profile_form(FlaskForm):
 
     submit = SubmitField()
 
+
 class user_edit_form(FlaskForm):
     about = TextAreaField('About', [validators.length(min=6, max=300)],
                           description="Please add information about yourself")
-    is_admin = BooleanField('Admin', render_kw={'value':'1'})
+    is_admin = BooleanField('Admin', render_kw={'value': '1'})
     submit = SubmitField()
 
 
@@ -74,6 +75,7 @@ class security_form(FlaskForm):
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
 
     submit = SubmitField()
+
 
 class csv_upload(FlaskForm):
     file = FileField()
