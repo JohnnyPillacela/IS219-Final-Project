7f384611e88f7494bb13907395616b948e6da65f
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-10 23:00:26 -0400

    made form for new item to add to db

--- app/simple_pages/forms/__init__.py
+++ app/simple_pages/forms/__init__.py
@@ -1,8 +1,26 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import SubmitField, TextAreaField, validators
 
 
 class create_product_form(FlaskForm):
-
+    name = TextAreaField('Product Name',
+                         [validators.DataRequired(), ],
+                         description="You need add a product name")
+    description = TextAreaField('Description',
+                                [validators.DataRequired(), ],
+                                description="You need to add a description")
+    price = TextAreaField('Price',
+                          [validators.DataRequired(), ],
+                          description="You need to add a price")
+    comments = TextAreaField('Comments',
+                             [validators.DataRequired(), ],
+                             description="You need to add a comment")
+    filename = TextAreaField('Link to Image',
+                             [validators.DataRequired(), ],
+                             description="Add an image to your product")
+    email = TextAreaField('Enter your email',
+                             [validators.DataRequired(), ],
+                             description="Add your email")
 
     submit = SubmitField()
