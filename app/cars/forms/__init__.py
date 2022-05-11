6e87b68925b9c534c9d6fa0b29116b1aaecd28b0
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-11 12:30:56 -0400

    updated car form

--- app/cars/forms/__init__.py
+++ app/cars/forms/__init__.py
@@ -3,22 +3,21 @@ from wtforms import SubmitField, TextAreaField, validators
 
 
 class create_car_form(FlaskForm):
-    name = TextAreaField('Car Name',
-                         [validators.DataRequired(), ],
-                         description="Name of the Car")
+    car_maker = TextAreaField('Car Maker',
+                              [validators.DataRequired(), ],
+                              description="Provide maker of the car")
     model = TextAreaField('Car Model',
-                             [validators.DataRequired(), ],
-                             description="You need add the model of the car")
+                          [validators.DataRequired(), ],
+                          description="You need add the model of the car")
     year = TextAreaField('Car Year',
-                             [validators.DataRequired(), ],
-                             description="Year of this Car")
+                         [validators.DataRequired(), ],
+                         description="Year of this Car")
     price = TextAreaField('Price',
                           [validators.DataRequired(), ],
                           description="You need add the price of this car")
     description = TextAreaField('Description',
-                                [validators.DataRequired(), ],
                                 description="You need to add any description of the car")
     image_link = TextAreaField('Link to Car Image',
-                             [validators.DataRequired(), ],
-                             description="Add an image to your Car")
+                               [validators.DataRequired(), ],
+                               description="Add an image to your Car")
     submit = SubmitField()
