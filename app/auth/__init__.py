04e6c3c18852c35315d2e674acf4d089ac64158c
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-05-10 23:30:24 -0400

    auth stuff

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Blueprint, render_template, redirect, url_for, flash, current_app, abort
+from flask import Blueprint, render_template, redirect, url_for, flash, current_app, abort, request
 from flask_login import login_user, login_required, logout_user, current_user
 from jinja2 import TemplateNotFound
 from sqlalchemy import select
@@ -44,18 +44,25 @@ def login():
     form = login_form()
     if current_user.is_authenticated:
         return redirect(url_for('auth.dashboard'))
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('auth.login'))
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user is None or not user.check_password(form.password.data):
+                flash('Invalid username or password')
+                return redirect(url_for('auth.login'))
+            else:
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                if user.id == 1:
+                    user.is_admin = 1
+                    db.session.add(user)
+                    db.session.commit()
+                login_user(user)
+                flash("Welcome", 'success')
+                return redirect(url_for('auth.dashboard'))
         else:
-            user.authenticated = True
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            flash("Welcome", 'success')
-            return redirect(url_for('auth.dashboard'))
+            flash('Invalid username or password')
     return render_template('login.html', form=form)
 
 @auth.route("/logout")
